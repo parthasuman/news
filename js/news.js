e66f2d4e9d4ff7edd816158785d0f45b559e636d
parthasuman <parthasuman123@gmail.com>
2022-09-04 20:03:20 +0600

    newjs1 rewrite

--- js/news.js
+++ js/news.js
@@ -8,17 +8,14 @@ const loadAllNews = async () => {
 };
 
 const setAllMenu = async () => {
-  // console.log(loadAllNews);
-  // loadAllNews().then((data) => console.log(data));
   const dataAll = await loadAllNews();
 
   const menu = document.getElementById("news-category");
 
   for (const new1 of dataAll) {
-    // console.log(new1.category_name);
     const li = document.createElement("li");
     li.innerHTML = `<button onclick="loadNewss(${new1.category_id})">${new1.category_name}</button>`;
-    // `<a>${new1.category_name}</a>`;
+
     menu.appendChild(li);
   }
 };
